75cd22914e9a2554e2c6ac4d18a7430972488ccf
anhbv <anhbuidustin@gmail.com>
2022-12-06 15:36:50 +0900

    feat: update

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,35 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type == "success") {
+        // Display a success toast, with a title
+        toastr.success(message, 'Operation Successful', { timeOut: 5000 })
+    }
+    if (type == "error") {
+        // Display a success toast, with a title
+        toastr.error(message, 'Operation failed', { timeOut: 5000 })
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type == "success") {
+        Swal.fire(
+            'Success Notification!',
+            message,
+            'success'
+        )
+    }
+    if (type == "error") {
+        Swal.fire(
+            'Success Notification!',
+            message,
+            'error'
+        )
+    }
+}
+
+function ShowDeleteConfirmationModal() {
+    $("#deleteConfirmationModal").modal("show");
+}
+
+function HideDeleteConfirmationModal() {
+    $("#deleteConfirmationModal").modal("hide");
+}
